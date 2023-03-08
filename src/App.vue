d53fc26d05fe4ff8220cab61626f4c4ba48a15ef
Curtis Tarr <30830713+CurtisTarr@users.noreply.github.com>
2023-03-08 20:55:55 +0000

    Fixing css for link buttons

--- src/App.vue
+++ src/App.vue
@@ -54,15 +54,12 @@ body {
   color: var(--color);
   margin: 0;
   padding: 0;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
+  position: center;
 }
 
 #links {
   max-width: 90%;
-  width: auto;
+  width: 250px;
   display: block;
   margin: 27px auto;
 }
